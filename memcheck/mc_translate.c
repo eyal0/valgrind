fdcde5b91eac75469313deb591180b7e14e5790a
eyal0 <109809+eyal0@users.noreply.github.com>
2021-03-01 18:13:34 -0700

    Add support for precise computation of SIMD greater than
    This fixes https://bugs.kde.org/show_bug.cgi?id=432801
    
    To test:
    
    ```sh
    make && perl tests/vg_regtest memcheck/tests/x86/pcmpgtd
    ```

--- memcheck/mc_translate.c
+++ memcheck/mc_translate.c
@@ -1287,6 +1287,101 @@ static IRAtom* expensiveCmpEQorNE ( MCEnv*  mce,
    return final_cast;
 }
 
+/* Check if we can know, despite the uncertain bits, that xx is greater than yy.
+   Notice that it's xx > yy and not the other way around.  This is Intel syntax
+   with destination first.  It will appear reversed in gdb disassembly (AT&T
+   syntax).
+ */
+static IRAtom* expensiveCmpGT ( MCEnv*  mce,
+                                unsigned int word_size,
+                                Bool is_signed,
+                                unsigned int count,
+                                IRAtom* vxx, IRAtom* vyy,
+                                IRAtom* xx,  IRAtom* yy )
+{
+   IROp   opAND, opOR, opXOR, opNOT, opEQ, opSHL, opGT;
+   IRType ty;
+
+   tl_assert(isShadowAtom(mce,vxx));
+   tl_assert(isShadowAtom(mce,vyy));
+   tl_assert(isOriginalAtom(mce,xx));
+   tl_assert(isOriginalAtom(mce,yy));
+   tl_assert(sameKindedAtoms(vxx,xx));
+   tl_assert(sameKindedAtoms(vyy,yy));
+
+   switch (word_size * count) {
+      case 128:
+         ty = Ity_V128;
+         opAND = Iop_AndV128;
+         opOR   = Iop_OrV128;
+         opXOR  = Iop_XorV128;
+         opNOT  = Iop_NotV128;
+         break;
+      default:
+         VG_(tool_panic)("expensiveCmpGT");
+   }
+   if (word_size == 32 && count == 4) {
+      opEQ = Iop_CmpEQ32x4;
+      opSHL = Iop_ShlN32x4;
+      if (is_signed) {
+         opGT = Iop_CmpGT32Sx4;
+      } else {
+         opGT = Iop_CmpGT32Ux4;
+      }
+   } else {
+      VG_(tool_panic)("expensiveCmpGT");
+   }
+   IRAtom *MSBs;
+   if (is_signed) {
+      // For unsigned it's easy to make the min and max: Just set the unknown
+      // bits all to 0s or 1s.  For signed it's harder because having a 1 in the
+      // MSB makes a number smaller, not larger!  We can work around this by
+      // flipping the MSB before and after computing the min and max values.
+      IRAtom *const0 = mkV128(0);
+      IRAtom *all_ones = assignNew('V', mce, ty, binop(opEQ, const0, const0));
+      MSBs = assignNew('V', mce, ty, binop(opSHL, all_ones, mkU8(31)));
+      xx = assignNew('V', mce, ty, binop(opXOR, xx, MSBs));
+      yy = assignNew('V', mce, ty, binop(opXOR, yy, MSBs));
+      // From here on out, we're dealing with MSB-flipped integers.
+   }
+   // We can combine xx and vxx to create two values: the largest that xx could
+   // possibly be and the smallest that xx could possibly be.  Likewise, we can
+   // do the same for yy.  We'll call those max_xx and min_xx and max_yy and
+   // min_yy.
+   IRAtom *not_vxx = assignNew('V', mce, ty, unop(opNOT, vxx));
+   IRAtom *not_vyy = assignNew('V', mce, ty, unop(opNOT, vyy));
+   IRAtom *max_xx = assignNew('V', mce, ty, binop(opOR, xx, vxx));
+   IRAtom *min_xx = assignNew('V', mce, ty, binop(opAND, xx, not_vxx));
+   IRAtom *max_yy = assignNew('V', mce, ty, binop(opOR, yy, vyy));
+   IRAtom *min_yy = assignNew('V', mce, ty, binop(opAND, yy, not_vyy));
+   if (is_signed) {
+      // Unflip the MSBs.
+      max_xx = assignNew('V', mce, ty, binop(opXOR, max_xx, MSBs));
+      min_xx = assignNew('V', mce, ty, binop(opXOR, min_xx, MSBs));
+      max_yy = assignNew('V', mce, ty, binop(opXOR, max_yy, MSBs));
+      min_yy = assignNew('V', mce, ty, binop(opXOR, min_yy, MSBs));
+   }
+   IRAtom *min_xx_gt_max_yy = assignNew('V', mce, ty, binop(opGT, min_xx, max_yy));
+   IRAtom *max_xx_gt_min_yy = assignNew('V', mce, ty, binop(opGT, max_xx, min_yy));
+   // If min_xx is greater than max_yy then xx is surely greater than yy so we know
+   // our answer for sure.  If max_xx is not greater than min_yy then xx can't
+   // possible be greater than yy so again we know the answer for sure.  For all
+   // other cases, we can't know.
+   //
+   // So the result is defined if:
+   //
+   // min_xx_gt_max_yy | ~max_xx_gt_min_yy
+   //
+   // Because defined in vbits is 0s and not 1s, we need to invert that:
+   //
+   // ~(min_xx_gt_max_yy | ~max_xx_gt_min_yy)
+   //
+   // We can use DeMorgan's Law to simplify the above:
+   //
+   // ~min_xx_gt_max_yy & max_xx_gt_min_yy
+   IRAtom *not_min_xx_gt_max_yy = assignNew('V', mce, ty, unop(opNOT, min_xx_gt_max_yy));
+   return assignNew('V', mce, ty, binop(opAND, not_min_xx_gt_max_yy, max_xx_gt_min_yy));
+}
 
 /* --------- Semi-accurate interpretation of CmpORD. --------- */
 
@@ -3964,9 +4059,13 @@ IRAtom* expr2vbits_Binop ( MCEnv* mce,
       case Iop_PwExtUSMulQAdd8x16:
          return binary16Ix8(mce, vatom1, vatom2);
 
-      case Iop_Sub32x4:
       case Iop_CmpGT32Sx4:
+         return expensiveCmpGT(mce, 32, True /* signed */,
+                               4, vatom1, vatom2, atom1, atom2);
       case Iop_CmpGT32Ux4:
+         return expensiveCmpGT(mce, 32, False /* unsigned */,
+                               4, vatom1, vatom2, atom1, atom2);
+      case Iop_Sub32x4:
       case Iop_CmpEQ32x4:
       case Iop_QAdd32Sx4:
       case Iop_QAdd32Ux4:
